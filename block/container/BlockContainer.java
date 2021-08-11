71d2293b8cb1fd5c697091c2d6fd731e288198e0
jasoncho1021 <jhcho1021@gmail.com>
2021-08-11 17:57:33 +0900

    seperate di package

---
+++ block/container/BlockContainer.java
@@ -0,0 +1,47 @@
+package tetris.block.container;
+
+import java.util.List;
+import java.util.Random;
+
+import tetris.block.Block;
+import tetris.block.TetrisBlock;
+
+public class BlockContainer {
+
+	private Random random;
+	private List<Class<?>> clazzList;
+
+	private BlockContainer() {
+		random = new Random();
+		scan(getClass().getPackage().getName());
+	}
+
+	public static BlockContainer getInstance() {
+		return LazyHolder.INSTANCE;
+	}
+
+	private static class LazyHolder {
+		private static final BlockContainer INSTANCE = new BlockContainer();
+	}
+
+	private void scan(String packageName) {
+		clazzList = DiScanner.scanPackageAndGetClass(packageName, TetrisBlock.class);
+	}
+
+	public Block getNewBlock() {
+		int idx = random.nextInt(clazzList.size());
+		Class<?> c = clazzList.get(idx);
+		try {
+			return (Block) c.newInstance();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
